9c472d515a2491d0c9827b98868991c0f1224eda
Chutong Lin <30555643+RobinLCT@users.noreply.github.com>
2019-10-08 17:07:30 -0500

    Add files via upload

---
+++ CleanSweep_Group6/src/main/java/edu/depaul/cleanSweep/controlSystem/Cleaner.java
@@ -0,0 +1,17 @@
+package edu.depaul.cleanSweep.controlSystem;
+
+
+
+
+public class Cleaner {
+
+	
+	private static final int MAX_DIRT_CAPACITY = 50;
+
+	public Cleaner() {
+		
+	}
+
+
+
+}
---
+++ CleanSweep_Group6/src/test/java/edu/depaul/cleanSweep/controlSystem/CleanerTest.java
@@ -0,0 +1,17 @@
+package edu.depaul.cleanSweep.controlSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+class CleanerTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
